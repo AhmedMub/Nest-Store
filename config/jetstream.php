701effde92c2e7e12580ea2187b1e5d892ca8276
AhmedMub <mada.mub@gmail.com>
2022-01-25 12:54:33 +0300

    push current changes fix admin/login

--- config/jetstream.php
+++ config/jetstream.php
@@ -58,7 +58,7 @@ return [
         // Features::termsAndPrivacyPolicy(),
         // Features::profilePhotos(),
         // Features::api(),
-        Features::teams(['invitations' => true]),
+        //Features::teams(['invitations' => true]),
         Features::accountDeletion(),
     ],
 
--- database/migrations/2020_05_21_100000_create_teams_table.php
+++ database/migrations/2020_05_21_100000_create_teams_table.php
@@ -16,6 +16,7 @@ class CreateTeamsTable extends Migration
         Schema::create('teams', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->index();
+            $table->foreignId('admin_id')->index()->nullable();
             $table->string('name');
             $table->boolean('personal_team');
             $table->timestamps();
