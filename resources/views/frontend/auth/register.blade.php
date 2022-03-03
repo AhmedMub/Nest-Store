b10c161631819394e9f5bc4297a4c83951151065
AhmedMub <mada.mub@gmail.com>
2022-03-03 14:21:23 +0300

    edit register view and frontRoutes

--- resources/views/frontend/auth/register.blade.php
+++ resources/views/frontend/auth/register.blade.php
@@ -4,7 +4,7 @@
     <div class="page-header breadcrumb-wrap">
         <div class="container">
             <div class="breadcrumb">
-                <a href="index.html" rel="nofollow"><i class="fi-rs-home mr-5"></i>Home</a>
+                <a href=" {{route('home')}} " rel="nofollow"><i class="fi-rs-home mr-5"></i>Home</a>
                 <span></span> Profile <span></span> Register
             </div>
         </div>
--- routes/frontend/frontendRoutes.php
+++ routes/frontend/frontendRoutes.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [FrontController::class, 'index']);
+Route::get('/', [FrontController::class, 'index'])->name('home');
 
 Route::middleware(['auth:sanctum', 'verified'])->group(function () {
 });
